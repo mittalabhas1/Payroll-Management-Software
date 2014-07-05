6490a90df76ad2e8d4ec6a0fabaefda1b4840f6b
Abhas Mittal <mittalabhas1@gmail.com>
2014-07-06 04:59:52 +0530

    feat(user): created viewsets

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, generics
+
+from users.serializers import UserSerializer
+from users.models import User
+
+class UserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    '''
+    Endpoint to get and update profile of a `User`.
+    '''
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
