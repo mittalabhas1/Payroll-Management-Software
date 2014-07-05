751d3e19b45a14d32b51c1370da0787a1dc80b92
Abhas Mittal <mittalabhas1@gmail.com>
2014-07-06 05:05:55 +0530

    feat(setup): made users.User base model for JWT authentication

--- payroll/settings.py
+++ payroll/settings.py
@@ -87,3 +87,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': (
+    'rest_framework.permissions.IsAuthenticated',
+  ),
+  'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework.authentication.SessionAuthentication',
+    'rest_framework.authentication.BasicAuthentication',
+    'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+  ),
+}
